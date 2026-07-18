d53b38c144b0dca52b53b5fe05868e91b709831c
Manish <mkthakur2301@gmail.com>
2026-07-18 10:34:42 +0530

    q1 of patter programming

---
+++ Pattern/q1.py
@@ -0,0 +1,14 @@
+# method-1
+# n=5
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print('* ', end='')
+#     print()
+
+# method-2
+# n=5
+# for i in range(1, n+1):
+#     row=''
+#     for j in range(1, n+1):
+#         row = row+'* '
+#     print(row)
