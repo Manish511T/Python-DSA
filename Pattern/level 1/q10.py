6b3ad397f7ab0b9ac5f93bba6f6053f6aebdbe44
Manish <mkthakur2301@gmail.com>
2026-07-20 23:22:43 +0530

    level 1 q10

---
+++ Pattern/level 1/q10.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+mid = (n//2)+1
+if n%2==0:
+    print("Enter odd number")
+else:
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i==mid or j==mid or i==1 or i==n or j==1 or j==n or i==j or i+j==n+1:
+                print('*', end=' ')
+            else:
+                print(" ", end=' ')
+        print()
