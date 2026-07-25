d3f2f5ee6a551ace826135ffc76c2746328d3ebb
Manish <mkthakur2301@gmail.com>
2026-07-25 13:04:32 +0530

    Level 2 q24.py

---
+++ Pattern/level 2/q24.py
@@ -0,0 +1,25 @@
+def printPattern(n):
+    mid = n//2+1
+    space = mid-1
+    patternSize = 1
+
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print("  ", end='')
+        for j in range(1, patternSize+1):
+            print('* ', end='')
+        if i<mid:
+            space -= 1
+            patternSize +=2
+        else:
+            space += 1
+            patternSize -=2
+        print()
+        
+
+
+n = int(input("Enter a number: "))
+if n%2==0:
+    print("Row must be odd number!")
+else:
+    printPattern(n)
