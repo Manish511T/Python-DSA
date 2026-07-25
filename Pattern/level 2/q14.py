a130f0a3881c88dd26d5c11d32dc044fdd02ee64
Manish <mkthakur2301@gmail.com>
2026-07-25 10:43:18 +0530

    level 2 q14

--- Pattern/level 2/q14.py
+++ Pattern/level 2/q14.py
@@ -1,13 +1,16 @@
 def printPattern(n):
-    patternSize = n
+    pattern_size = 1
+    mid = n//2+1
+
     for i in range(1, n+1):
-        for j in range(1,patternSize+1):
-            print(" ", end=' ')
-        for j in range(1, i+1):
+        for j in range(1, pattern_size):
             print('*', end=' ')
-        patternSize -=1
+        if i<mid:
+            pattern_size +=1
+        else:
+            pattern_size -=1
         print()
-        
+
 
 n = int(input("Enter a number: "))
 printPattern(n)
---
+++ Pattern/level 2/q_14.py
@@ -0,0 +1,13 @@
+def printPattern(n):
+    patternSize = n
+    for i in range(1, n+1):
+        for j in range(1,patternSize+1):
+            print(" ", end=' ')
+        for j in range(1, i+1):
+            print('*', end=' ')
+        patternSize -=1
+        print()
+        
+
+n = int(input("Enter a number: "))
+printPattern(n)
