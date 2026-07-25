50d3df44048473f5c2e055b53a6face900ec3bb7
Manish <mkthakur2301@gmail.com>
2026-07-25 12:43:29 +0530

    level 2 q21

---
+++ Pattern/level 2/q21.py
@@ -0,0 +1,29 @@
+'''
+Enter a number: 7
+*   *   *   *   *   *   *   
+  *   *   *   *   *   *   
+    *   *   *   *   *   
+      *   *   *   *   
+        *   *   *   
+          *   *   
+            *   
+'''
+
+
+def printPattern(n):
+    space = 0
+    patternSize = n
+
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print('  ', end='')
+        for j in range(1, patternSize+1):
+            print('*   ', end='')
+        space += 1
+        patternSize -=1
+        print()
+        
+
+
+n = int(input("Enter a number: "))
+printPattern(n)
