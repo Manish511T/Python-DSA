54f999088980a7650cd013e7c44638caebb591b9
Manish <mkthakur2301@gmail.com>
2026-07-25 11:59:12 +0530

    level 2 q19

---
+++ Pattern/level 2/q19.py
@@ -0,0 +1,29 @@
+'''
+Enter a number: 7
+            *   
+          *   *   
+        *   *   *   
+      *   *   *   *   
+    *   *   *   *   *   
+  *   *   *   *   *   *   
+*   *   *   *   *   *   * 
+'''
+
+
+def printPattern(n):
+    space = n-1
+    patternSize = 1
+
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print(' ', end=' ')
+        for j in range(1, patternSize+1):
+            print('*', end='   ')
+        space -= 1
+        patternSize +=1
+        print()
+        
+
+
+n = int(input("Enter a number: "))
+printPattern(n)
