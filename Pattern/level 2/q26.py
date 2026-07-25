dcb793d283667f60ffcfdd8ace3e54a4c0c67e4b
Manish <mkthakur2301@gmail.com>
2026-07-25 13:19:29 +0530

    level 2 q26

---
+++ Pattern/level 2/q26.py
@@ -0,0 +1,36 @@
+'''
+Enter a number: 9
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+'''
+
+def printPattern(n):
+    mid = n//2+1
+    space = 0
+    patternSize = mid
+
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print("  ", end='')
+        for j in range(1, patternSize+1):
+            print("* ", end='')
+        if i<mid:
+            patternSize -=1
+            space +=1
+        else:
+            patternSize +=1
+            space -=1
+        print()
+
+n = int(input("Enter a number: "))
+if n%2==0:
+    print("Row must be odd number!")
+else:
+    printPattern(n)
