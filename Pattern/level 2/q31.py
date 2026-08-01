c2f418670318451cc3dc7efacc9ac3c34ac70660
Manish <mkthakur2301@gmail.com>
2026-08-01 11:18:10 +0530

    level 2 q31

---
+++ Pattern/level 2/q31.py
@@ -0,0 +1,35 @@
+'''
+Enter a number: 9
+* * * * * * * * * 
+* * * *   * * * * 
+* * *       * * * 
+* *           * * 
+*               * 
+* *           * * 
+* * *       * * * 
+* * * *   * * * * 
+* * * * * * * * *
+'''
+def print_pattern(n):
+    mid = n//2+1
+    start = mid
+    end = mid
+    for i in range(1,n+1):
+        for j in range(1, n+1):
+            if j==start or j==end or j<=start or j>=end :
+                print("* ", end='')
+            else:
+                print("  ", end='')
+        print()
+        if i<mid:
+            start -=1
+            end +=1
+        else:
+            start +=1
+            end -=1
+
+n = int(input("Enter a number: "))
+if n%2==0:
+    print("Enter odd number only!!!")
+else:
+    print_pattern(n)
