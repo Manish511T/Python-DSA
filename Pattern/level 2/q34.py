c17f6d0ce1cb04e256342f6b41bc83d6fb3f9d7c
Manish <mkthakur2301@gmail.com>
2026-08-01 12:30:14 +0530

    level 2 q34

---
+++ Pattern/level 2/q34.py
@@ -0,0 +1,21 @@
+def print_pattern(n):
+    mid = n//2+1
+    start = mid
+    end = mid
+    for i in range(1,n+1):
+        for j in range(1, n+1):
+            if j<=start or j>=end :
+                print("* ", end='')
+            else:
+                print("  ", end='')
+        print()
+        if i<mid:
+            start -=1
+            end +=1
+
+
+n = int(input("Enter a number: "))
+if n%2==0:
+    print("Enter odd number only!!!")
+else:
+    print_pattern(n)
