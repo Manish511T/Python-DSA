7d863630e1a11957432397307848891eb159f25d
Manish <mkthakur2301@gmail.com>
2026-08-06 11:35:25 +0530

    level2 q44

--- Pattern/level 2/q44.py
+++ Pattern/level 2/q44.py
@@ -1,22 +1,21 @@
 '''
 Enter a number: 5
-1  
-2  3  
-4  5  6  
-7  8  9  10  
-11  12  13  14  15  
+15  
+14  13  
+12  11  10  
+9  8  7  6  
+5  4  3  2  1  
 '''
 
 def print_pattern(n):
     pattern_size = 1
-    num = 1
+    num = n*3
     for i in range(1, n+1):
         for j in range(1, pattern_size+1):
             print(num, ' ', end='')
-            num +=1
+            num -=1
         pattern_size+=1
         print()
-
 
 n = int(input("Enter a number: "))
 print_pattern(n)
