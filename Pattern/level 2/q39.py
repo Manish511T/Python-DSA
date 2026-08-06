1d327a95f9f76f38b42c191549cc900f6a03f7e2
Manish <mkthakur2301@gmail.com>
2026-08-06 11:08:49 +0530

    level 2 q39

--- Pattern/level 2/q39.py
+++ Pattern/level 2/q39.py
@@ -1,14 +1,32 @@
 '''
-
+Enter a number: 7
+1  
+1  2  
+1  2  3  
+1  2  3  4  
+1  2  3  4  5  
+1  2  3  4  5  6  
+1  2  3  4  5  6  7  
 '''
 
 
+# def print_pattern(n):
+#     num = 1
+#     for i in range(1, n+1):
+#         for j in range(num):
+#             print(num,' ', end='')
+#         num +=1
+#         print()
+
 def print_pattern(n):
-    num = 1
+    pattern_size = 1
     for i in range(1, n+1):
-        for j in range(num):
-            print(num,' ', end='')
-        num +=1
+        num = 1
+        for j in range(1, pattern_size+1):
+            print(num,' ',end='')   
+            num +=1 
+        pattern_size +=1
+        
         print()
 
 
