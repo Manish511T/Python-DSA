734c389355cff88aad037b86cf16761fca122fad
Manish <mkthakur2301@gmail.com>
2026-08-06 12:39:39 +0530

    level 2 q45 and q46

--- Pattern/level 2/q45.py
+++ Pattern/level 2/q45.py
@@ -1,20 +1,21 @@
 '''
 Enter a number: 5
-15  
-14  13  
-12  11  10  
-9  8  7  6  
-5  4  3  2  1  
+1 
+3       2 
+6       5       4 
+10      9       8       7 
+15      14      13      12      11 
 '''
-
 def print_pattern(n):
     pattern_size = 1
-    num = n*(n+1)//2
+    num = 1
     for i in range(1, n+1):
+        num += pattern_size - 1
         for j in range(1, pattern_size+1):
             print(num, '\t', end='')
             num -=1
         pattern_size+=1
+        num +=pattern_size
         print()
 
 n = int(input("Enter a number: "))
--- Pattern/level 2/q46.py
+++ Pattern/level 2/q46.py
@@ -1,20 +1,20 @@
 '''
 Enter a number: 5
-15  
-14  13  
-12  11  10  
-9  8  7  6  
-5  4  3  2  1  
+15 
+13      14 
+10      11      12 
+6       7       8       9 
+1       2       3       4       5 
 '''
-
 def print_pattern(n):
     pattern_size = 1
     num = n*(n+1)//2
     for i in range(1, n+1):
         for j in range(1, pattern_size+1):
             print(num, '\t', end='')
-            num -=1
-        pattern_size+=1
+            num +=1
+        num = num - (2*i+1)
+        pattern_size +=1
         print()
 
 n = int(input("Enter a number: "))
