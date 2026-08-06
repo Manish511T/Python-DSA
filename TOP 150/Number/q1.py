f3da49b8f9b6fe0f261ed51e7cd9a72010f18ee7
Manish <mkthakur2301@gmail.com>
2026-08-06 11:46:28 +0530

    Top 150, Number q1

---
+++ TOP 150/Number/q1.py
@@ -0,0 +1,21 @@
+'''
+a) WAJP to swap two numbers using a third
+variable.
+b) WAJP to swap two numbers without using a
+third variable.
+'''
+
+# a) WAJP to swap two numbers using a third
+# variable.
+
+a = 2
+b = 3
+print("Before Swape")
+print(a)
+print(b)
+temp = a
+a = b
+b = temp
+print("After Swape")
+print(a)
+print(b)
