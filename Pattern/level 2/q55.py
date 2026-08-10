608cfd2a5d2ee93bf05b3d3adfe7aa526881bbae
Manish <mkthakur2301@gmail.com>
2026-08-10 23:29:10 +0530

    level 2 q55

---
+++ Pattern/level 2/q55.py
@@ -0,0 +1,28 @@
+'''
+Enter a size of row: 7
+                  1  
+               1  2  
+            1  2  3  
+         1  2  3  4  
+      1  2  3  4  5  
+   1  2  3  4  5  6  
+1  2  3  4  5  6  7  
+'''
+
+
+def print_pattern(n):
+    pattern_size = 1
+    space = n-1
+
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print(' ',' ', end='')
+        for j in range(1, pattern_size+1):
+            print(j,' ', end='')
+        space -=1
+        pattern_size+=1
+        print()
+        
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
