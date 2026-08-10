e4f19ce2775808f94b1669627e0e37bc4a04552c
Manish <mkthakur2301@gmail.com>
2026-08-10 23:13:23 +0530

    level 2 q50

---
+++ Pattern/level 2/q50.py
@@ -0,0 +1,20 @@
+'''
+Enter a size of row: 5
+A 
+A       B 
+A       B       C 
+A       B       C       D 
+A       B       C       D       E 
+'''
+
+def print_pattern(n):
+    pattern_size = 1
+
+    for i in range(1, n+1):
+        for j in range(1, pattern_size+1):
+            print(chr(64+j), '\t', end='')
+        pattern_size +=1
+        print()
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
