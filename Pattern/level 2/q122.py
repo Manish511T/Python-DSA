3d3ee8bfe967a58d401a28485ae9e38d55986377
Manish <mkthakur2301@gmail.com>
2026-08-12 00:59:04 +0530

    level 2 q122

---
+++ Pattern/level 2/q122.py
@@ -0,0 +1,26 @@
+'''
+Enter a size of row: 5
+O               N               M               L               K 
+        J               I               H               G 
+                F               E               D 
+                        C               B 
+                                A 
+'''
+
+def print_pattern(n):
+    pattern_size = n
+    space = 0
+    num = n*(n+1)//2
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print('\t', end='')
+        for j in range(1, pattern_size+1):
+            print(chr(64+num), '\t\t', end='')
+            num -=1
+        pattern_size -=1
+        space +=1
+        print()
+
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
