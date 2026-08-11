16ee8b486181bfd3bca8ba65f07e98345a4eaf10
Manish <mkthakur2301@gmail.com>
2026-08-11 11:40:15 +0530

    level 2 q101

---
+++ Pattern/level 2/q101.py
@@ -0,0 +1,33 @@
+'''
+Enter a size of row: 5
+                                1   
+                        2       3       2   
+                4       5       6       5       4   
+        7       8       9       10      9       8       7   
+11      12      13      14      15      14      13      12      11  
+'''
+
+def print_pattern(n):
+    pattern_size = 1
+    space = n-1
+    num = 1
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print('\t', end='')
+
+        colmid = pattern_size//2+1
+        for j in range(1, pattern_size+1):
+            
+            if j<colmid:
+                print(num,'  \t',end='')
+                num +=1
+            else:
+                print(num,'  \t',end='')
+                num -=1
+        num = num + i + 1
+        space -=1
+        pattern_size +=2
+        print()
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
