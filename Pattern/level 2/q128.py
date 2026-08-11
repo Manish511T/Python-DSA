e52ff798237a7a050ed2f81eeef312a8cfac3d6d
Manish <mkthakur2301@gmail.com>
2026-08-12 01:14:48 +0530

    level 2 q128

---
+++ Pattern/level 2/q128.py
@@ -0,0 +1,27 @@
+'''
+Enter a size of row: 5
+1    0    1    0    1    0    1    0    1    
+     1    0    1    0    1    0    1    
+          1    0    1    0    1    
+               1    0    1    
+                    1
+'''
+
+def print_pattern(n):
+    pattern_size = 2*n-1
+    space = 0
+    
+    for i in range(1, n+1):
+        num = 1
+        for j in range(1, space+1):
+            print(' ','   ', end='')
+        for j in range(1, pattern_size+1):
+            print(num%2, '   ', end='')
+            num +=1
+        pattern_size -=2
+        space +=1
+        print()
+
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
