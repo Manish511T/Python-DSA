80189218a1ab31c9d122cdfa65ad28f516432014
Manish <mkthakur2301@gmail.com>
2026-08-11 12:25:49 +0530

    level 2 q113

---
+++ Pattern/level 2/q113.py
@@ -0,0 +1,25 @@
+'''
+Enter a size of row: 5
+        1               1               1               1               1
+                2               2               2               2
+                        3               3               3
+                                4               4
+                                        5
+'''
+
+
+def print_pattern(n):
+    pattern_size = n
+    space = 1
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print("\t", end='')
+        for j in range(1, pattern_size+1):
+            print(i ,end='\t\t')
+        pattern_size -=1
+        space +=1
+        print()
+
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
