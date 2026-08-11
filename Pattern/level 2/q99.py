7bae33da97c3b9133dab83bb14e914ff0d02d7bf
Manish <mkthakur2301@gmail.com>
2026-08-11 11:17:33 +0530

    level 2 q99

---
+++ Pattern/level 2/q99.py
@@ -0,0 +1,32 @@
+'''
+Enter a size of row: 5
+                                0   
+                        1       0       1   
+                2       1       0       1       2   
+        3       2       1       0       1       2       3   
+4       3       2       1       0       1       2       3       4   
+'''
+
+def print_pattern(n):
+    pattern_size = 1
+    space = n-1
+    num = 0
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print('\t', end='')
+
+        colmid = pattern_size//2+1
+        for j in range(1, pattern_size+1):
+            
+            if j<colmid:
+                print(num,'  \t',end='')
+                num -=1
+            else:
+                print(num,'  \t',end='')
+                num +=1
+        space -=1
+        pattern_size +=2
+        print()
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
