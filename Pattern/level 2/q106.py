9265283c5b2e900759321a187a00c7c19694b22b
Manish <mkthakur2301@gmail.com>
2026-08-11 11:59:44 +0530

    level 2 q106

---
+++ Pattern/level 2/q106.py
@@ -0,0 +1,26 @@
+'''
+Enter a size of row: 5
+                                A   
+                        B       B       B   
+                C       C       C       C       C   
+        D       D       D       D       D       D       D   
+E       E       E       E       E       E       E       E       E  
+'''
+
+def print_pattern(n):
+    pattern_size = 1
+    space = n-1
+    
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print("\t", end='')
+        for j in range(1, pattern_size+1):
+            print(chr(64+i),'  \t', end='')
+        
+        space -=1
+        pattern_size+=2
+        print()
+
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
