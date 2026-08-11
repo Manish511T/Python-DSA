0cb036f969528625f24704661493cc09669012cc
Manish <mkthakur2301@gmail.com>
2026-08-12 00:46:06 +0530

    level 2 q116

---
+++ Pattern/level 2/q116.py
@@ -0,0 +1,24 @@
+'''
+Enter a size of row: 5
+1   0   1   0   1   
+  1   0   1   0   
+    1   0   1   
+      1   0   
+        1   
+'''
+
+def print_pattern(n):
+    pattern_size = n
+    space = 0
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print("  ", end='')
+        for j in range(1, pattern_size+1):
+            print(j%2, '  ', end='')
+        pattern_size -=1
+        space +=1
+        print()
+
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
