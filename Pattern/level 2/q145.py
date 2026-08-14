0f4e133e74780a6f542d7963b106a8d0c89d757a
Manish <mkthakur2301@gmail.com>
2026-08-15 00:38:23 +0530

    level 2 q145

---
+++ Pattern/level 2/q145.py
@@ -0,0 +1,33 @@
+'''
+Enter no. of row: 7
+      1   
+    2   2   
+  3   3   3   
+4   4   4   4   
+  5   5   5   
+    6   6   
+      7  
+'''
+def print_pattern(n):
+    if n%2 ==0 :
+        print("Odd number only! ")
+        return
+    pattern_size = 1
+    mid = n//2+1
+    space = mid-1
+
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print('  ', end='')
+        for j in range(1, pattern_size+1):
+            print(i,'  ', end='')
+        if i<mid:
+            space -=1
+            pattern_size +=1
+        else :
+            space +=1
+            pattern_size -=1
+        print()
+
+n = int(input("Enter no. of row: "))
+print_pattern(n)
