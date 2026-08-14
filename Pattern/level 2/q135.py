7815d52be837dfe607724a971b9e1944671a8c4b
Manish <mkthakur2301@gmail.com>
2026-08-14 11:27:42 +0530

    level 135

---
+++ Pattern/level 2/q135.py
@@ -0,0 +1,31 @@
+'''
+Enter a size of row: 5
+4       3       2       1       0       1       2       3       4  
+        3       2       1       0       1       2       3  
+                2       1       0       1       2  
+                        1       0       1  
+                                0 
+'''
+
+def print_pattern(n):
+    pattern_size = 2*n-1
+    space = 0
+    for i in range(1, n+1):
+        num = n-i
+        for j in range(1, space+1):
+            print(' ','\t', end='')
+        colmid = pattern_size//2+1
+        for j in range(1, pattern_size+1):
+            if j<colmid:
+                print(num, ' \t', end='')
+                num -=1
+            else:
+                print(num, ' \t', end='')
+                num +=1
+        
+        pattern_size -=2
+        space +=1
+        print()
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
