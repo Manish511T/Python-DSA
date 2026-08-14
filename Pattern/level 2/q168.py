8c36fa323c7a237849c40ec053d2039d7c52bfdb
Manish <mkthakur2301@gmail.com>
2026-08-15 00:56:21 +0530

    level 2 q168

--- Pattern/level 2/q168.py
+++ Pattern/level 2/q168.py
@@ -1,14 +1,13 @@
 '''
 Enter no. of row: 7
-                        7 
-                6       6       6 
-        5       5       5       5       5 
-4       4       4       4       4       4       4 
-        3       3       3       3       3 
-                2       2       2 
-                        1
+                        1 
+                3       2       1 
+        5       4       3       2       1 
+7       6       5       4       3       2       1 
+        5       4       3       2       1 
+                3       2       1 
+                        1 
 '''
-
 def print_pattern(n):
     if n%2 ==0 :
         print("Odd number only! ")
@@ -16,15 +15,13 @@ def print_pattern(n):
     pattern_size = 1
     mid = n//2+1
     space = mid-1
-    num = n
-
     for i in range(1, n+1):
+        num = pattern_size
         for j in range(1, space+1):
             print('\t', end='')
         for j in range(1, pattern_size+1):
             print(num, '\t', end='')
-
-        num -=1
+            num -=1
         if i<mid:
             pattern_size +=2
             space -=1
