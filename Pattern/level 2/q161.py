08c982255f76efacc631e454148b35391ff10090
Manish <mkthakur2301@gmail.com>
2026-08-15 00:43:25 +0530

    level 2 q161

---
+++ Pattern/level 2/q161.py
@@ -0,0 +1,34 @@
+'''
+Enter no. of row: 7
+                        1 
+                2       2       2 
+        3       3       3       3       3 
+4       4       4       4       4       4       4 
+        5       5       5       5       5 
+                6       6       6 
+                        7 
+'''
+def print_pattern(n):
+    if n%2 ==0 :
+        print("Odd number only! ")
+        return
+    pattern_size = 1
+    mid = n//2+1
+    space = mid-1
+    num = 1
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print('\t', end='')
+        for j in range(1, pattern_size+1):
+            print(num,'\t', end='')
+        num +=1
+        if i<mid:
+            space -=1
+            pattern_size +=2
+        else :
+            space +=1
+            pattern_size -=2
+        print()
+
+n = int(input("Enter no. of row: "))
+print_pattern(n)
