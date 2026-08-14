3ad2d5360d2a16a17937cfc6d430930169b05392
Manish <mkthakur2301@gmail.com>
2026-08-14 11:28:27 +0530

    level 2 q136

---
+++ Pattern/level 2/q136.py
@@ -0,0 +1,30 @@
+'''
+Enter a size of row: 5
+5       4       3       2       1       2       3       4       5  
+        4       3       2       1       2       3       4  
+                3       2       1       2       3  
+                        2       1       2  
+                                1  
+'''
+def print_pattern(n):
+    pattern_size = 2*n-1
+    space = 0
+    for i in range(1, n+1):
+        num = n-i+1
+        for j in range(1, space+1):
+            print(' ','\t', end='')
+        colmid = pattern_size//2+1
+        for j in range(1, pattern_size+1):
+            if j<colmid:
+                print(num, ' \t', end='')
+                num -=1
+            else:
+                print(num, ' \t', end='')
+                num +=1
+        
+        pattern_size -=2
+        space +=1
+        print()
+
+n = int(input("Enter a size of row: "))
+print_pattern(n)
