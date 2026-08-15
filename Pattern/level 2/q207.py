aa0c18cba4fd5cb002132a606e0ba719c8b9cdf1
Manish <mkthakur2301@gmail.com>
2026-08-15 19:25:07 +0530

    level 2 q207

---
+++ Pattern/level 2/q207.py
@@ -0,0 +1,38 @@
+'''
+Enter no. of row: 7
+7       6       5       4       3       2       1 
+        5       4       3       2       1 
+                3       2       1 
+                        1 
+                3       2       1 
+        5       4       3       2       1 
+7       6       5       4       3       2       1 
+'''
+def print_pattern(n):
+    if n%2==0:
+        print("Odd number only")
+    pattern_size = n
+    mid = n//2+1
+    space = 0
+    
+    for i in range(1, n+1):
+        num = pattern_size
+        for j in range(1, space+1):
+            print('\t', end='')
+        for j in range(1, pattern_size+1):
+            print(num, '\t', end='')
+            num -=1
+        
+        if i<mid:
+
+            pattern_size -=2
+            space +=1
+        else:
+            pattern_size +=2
+            space -=1
+        print()
+
+
+        
+n = int(input("Enter no. of row: "))
+print_pattern(n)
