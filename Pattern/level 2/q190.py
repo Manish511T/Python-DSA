58dd9d36bcb06d1a28fdaaecfb03cb87500ccbb1
Manish <mkthakur2301@gmail.com>
2026-08-15 14:34:40 +0530

    level 2 q190

---
+++ Pattern/level 2/q190.py
@@ -0,0 +1,37 @@
+'''
+Enter no. of row: 7
+                        A 
+                B       B       B 
+        C       C       C       C       C 
+D       D       D       D       D       D       D 
+        C       C       C       C       C 
+                B       B       B 
+                        A 
+'''
+def print_pattern(n):
+    if n%2==0:
+        print("Odd number only")
+    pattern_size = 1
+    mid = n//2+1
+    space = mid-1
+    num = 1
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print('\t', end='')
+        colmid = pattern_size//2+1
+        for j in range(1, pattern_size+1):
+            print(chr(64+num), '\t', end='')
+        
+        if i<mid:
+            num +=1
+            pattern_size +=2
+            space -=1
+        else:
+            num -=1
+            pattern_size -=2
+            space +=1
+        print()
+
+
+n = int(input("Enter no. of row: "))
+print_pattern(n)
