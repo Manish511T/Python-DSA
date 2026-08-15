9aae9203d55d4ad9ee55dbf174ae858e51c7c7a7
Manish <mkthakur2301@gmail.com>
2026-08-15 17:25:09 +0530

    level 2 q196

--- Pattern/level 2/q196.py
+++ Pattern/level 2/q196.py
@@ -1,27 +1,27 @@
 '''
 Enter no. of row: 7
-1       1       1       1       1       1       1 
-        0       0       0       0       0 
-                1       1       1 
-                        0 
-                1       1       1 
-        0       0       0       0       0 
-1       1       1       1       1       1       1 
+1       2       3       4       5       6       7 
+        1       2       3       4       5 
+                1       2       3 
+                        1 
+                1       2       3 
+        1       2       3       4       5 
+1       2       3       4       5       6       7
 '''
-
 def print_pattern(n):
     if n%2==0:
         print("Odd number only")
     pattern_size = n
     mid = n//2+1
     space = 0
-    num = 1
+
     for i in range(1, n+1):
+        num = 1
         for j in range(1, space+1):
             print('\t', end='')
         for j in range(1, pattern_size+1):
-            print(num%2, '\t', end='')
-        num +=1
+            print(num, '\t', end='')
+            num+=1
 
         if i<mid:
             pattern_size -=2
