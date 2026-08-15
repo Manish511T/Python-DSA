a48516fe7e45c801c5c071b449a115a2fcbe0a41
Manish <mkthakur2301@gmail.com>
2026-08-15 14:48:24 +0530

    level 2 q193

--- Pattern/level 2/q193.py
+++ Pattern/level 2/q193.py
@@ -1,11 +1,11 @@
 '''
 Enter no. of row: 7
                         A 
-                A       B       A 
-        A       B       C       B       A 
-A       B       C       D       C       B       A 
-        A       B       C       B       A 
-                A       B       A 
+                B       A       B 
+        C       B       A       B       C 
+D       C       B       A       B       C       D 
+        C       B       A       B       C 
+                B       A       B 
                         A 
 '''
 def print_pattern(n):
@@ -14,24 +14,24 @@ def print_pattern(n):
     pattern_size = 1
     mid = n//2+1
     space = mid-1
-    
+    num = 1
     for i in range(1, n+1):
-        num = 1
         for j in range(1,space+1):
             print('\t', end='')
         colmid = pattern_size//2+1
         for j in range(1, pattern_size+1):
             if j<colmid:
                 print(chr(64+num), '\t', end='')
-                num +=1
+                num -=1
             else:
                 print(chr(64+num), '\t', end='')
-                num -=1
+                num +=1
 
         if i<mid:
             pattern_size +=2
             space -=1
         else:
+            num -=2
             pattern_size -=2
             space +=1
         print()
