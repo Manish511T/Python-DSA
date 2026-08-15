9a7892a8df53efb0a0f1448b079599253dc4be12
Manish <mkthakur2301@gmail.com>
2026-08-15 14:07:01 +0530

    level 2 q182

---
+++ Pattern/level 2/q182.py
@@ -0,0 +1,37 @@
+'''
+Enter no. of row: 7
+                        * 
+                @       *       @ 
+        @       @       *       @       @ 
+*       *       *       *       *       *       * 
+        @       @       *       @       @ 
+                @       *       @ 
+                        * 
+'''
+def print_pattern(n):
+    if n%2==0:
+        print("Odd number only")
+    pattern_size = 1
+    mid = n//2+1
+    space = mid-1
+
+    for i in range(1, n+1):
+        for j in range(1,space+1):
+            print('\t', end='')
+        colmid = pattern_size//2+1
+        for j in range(1, pattern_size+1):
+            if i==mid or j==colmid:
+                print('*', '\t', end='')
+            else:
+                print('@', '\t', end='')
+        if i<mid:
+            pattern_size +=2
+            space -=1
+        else:
+            pattern_size -=2
+            space +=1
+        print()
+
+
+n = int(input("Enter no. of row: "))
+print_pattern(n)
