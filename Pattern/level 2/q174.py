81ee1f1efb107db30af5bcf75409d0b0e1a934a3
Manish <mkthakur2301@gmail.com>
2026-08-15 13:20:17 +0530

    level 2 q174

---
+++ Pattern/level 2/q174.py
@@ -0,0 +1,37 @@
+
+def print_pattern(n):
+    if n % 2 == 0:
+        print("Odd number only!")
+        return
+
+    pattern_size = 1
+    mid = n // 2 + 1
+    space = mid - 1
+    num = 1
+    for i in range(1, n + 1):
+        for j in range(1, space + 1):
+            print('\t', end='')
+
+        colmid = pattern_size // 2 + 1
+
+        for j in range(1, pattern_size + 1):
+            if j < colmid:
+                print(num, '\t', end='')
+                num -= 1
+            else:
+                print(num, '\t', end='')
+                num += 1
+
+        if i < mid:
+            pattern_size += 2
+            space -= 1
+        else:
+            num -=2
+            pattern_size -= 2
+            space += 1
+
+        print()
+
+
+n = int(input("Enter no. of row: "))
+print_pattern(n)
