89d8962e08ff736f642214fa010fd17bc510cfdd
Manish <mkthakur2301@gmail.com>
2026-08-15 19:18:35 +0530

    level 2 q204

--- Pattern/level 2/q204.py
+++ Pattern/level 2/q204.py
@@ -1,12 +1,12 @@
 '''
 Enter no. of row: 7
-1       1       1       1       1       1       1 
+3       3       3       3       3       3       3 
         2       2       2       2       2 
-                3       3       3 
-                        4 
-                3       3       3 
+                1       1       1 
+                        0 
+                1       1       1 
         2       2       2       2       2 
-1       1       1       1       1       1       1 
+3       3       3       3       3       3       3 
 '''
 def print_pattern(n):
     if n%2==0:
@@ -14,20 +14,19 @@ def print_pattern(n):
     pattern_size = n
     mid = n//2+1
     space = 0
-    num = 1
+    num = mid-1
     for i in range(1, n+1):
         for j in range(1, space+1):
             print('\t', end='')
         for j in range(1, pattern_size+1):
             print(num, '\t', end='')
         
-
         if i<mid:
-            num +=1
+            num -=1
             pattern_size -=2
             space +=1
         else:
-            num -=1
+            num +=1
             pattern_size +=2
             space -=1
         print()
