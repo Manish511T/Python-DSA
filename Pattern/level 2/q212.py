b750465de677b43428386bf2328c9e5c0a5da217
Manish <mkthakur2301@gmail.com>
2026-08-16 18:59:00 +0530

    level 2 q212

---
+++ Pattern/level 2/q212.py
@@ -0,0 +1,24 @@
+def print_pattern(n):
+    if n%2==0:
+        print("Odd number only")
+    start = 1
+    end = n
+    mid = n//2+1
+
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if j<=start or j>=end:
+                print(i,'\t', end='')
+            else:
+                print('  ','\t', end='')
+        print()
+        if i<mid:
+            start +=1
+            end -=1
+        else:
+            start -=1
+            end +=1
+
+n = int(input("Enter no. of row: "))
+print_pattern(n)
+
