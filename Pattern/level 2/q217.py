cb189906b92af2893c4e326322ebf5d7b5e63c7e
Manish <mkthakur2301@gmail.com>
2026-08-16 20:54:38 +0530

    fix: level 2 q217

--- Pattern/level 2/q217.py
+++ Pattern/level 2/q217.py
@@ -1,41 +1,50 @@
 '''
 Enter no. of row: 7
-1                                               2 
-1       2                               3       2 
-1       2       3               4       3       2 
-1       2       3       4       5       4       3 
-1       2       3               4       3       2 
-1       2                               3       2 
-1                                               2 
+1                                               1 
+1       2                               2       1 
+1       2       3               3       2       1 
+1       2       3       4       3       2       1 
+1       2       3               3       2       1 
+1       2                               2       1 
+1                                               1
 '''
 def print_pattern(n):
-    if n%2==0:
+    if n % 2 == 0:
         print("Odd number only!")
         return
 
     start = 1
     end = n
-    mid = n//2+1
+    mid = n // 2 + 1
 
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         num = 1
-        for j in range(1, n+1):
-            if j<=start:
+
+        for j in range(1, n + 1):
+
+            if j == start and j == end:
                 print(num, '\t', end='')
-                num +=1
-            elif j>=end:
+
+            elif j <= start:
                 print(num, '\t', end='')
-                num -=1
+                num += 1
+
+            elif j >= end:
+                print(num - 1, '\t', end='')
+                num -= 1
+
             else:
                 print(' ', '\t', end='')
 
-        if i<mid:
-            start +=1
-            end -=1
+        if i < mid:
+            start += 1
+            end -= 1
         else:
-            start -=1
-            end +=1
+            start -= 1
+            end += 1
+
         print()
+
 
 n = int(input("Enter no. of row: "))
 print_pattern(n)
