d1fee91c53a4bf501a40dd56a7b0478b6be1d23c
Manish <mkthakur2301@gmail.com>
2026-08-17 01:42:13 +0530

    level 2 q236

---
+++ Pattern/level 2/q236.py
@@ -0,0 +1,37 @@
+'''
+Enter no. of rows: 7
+                        1 
+                2               2 
+        3                               3 
+4                                               4 
+        3                               3 
+                2               2 
+                        1 
+'''
+def print_pattern(n):
+    if n%2==0:
+        print("Enter only odd number!")
+        return
+    mid = n//2+1
+    start = mid
+    end = mid
+    num = 1
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if j==start or j==end:
+                print(num, '\t', end='')
+            else:
+                print('\t', end='')
+
+        if i<mid:
+            num +=1
+            start -=1
+            end +=1
+        else:
+            num -=1
+            start +=1
+            end -=1
+        print()
+
+n = int(input("Enter no. of rows: "))
+print_pattern(n)
