f768d8b16eb44ea46d7a11c466e84e5dce86edc6
Manish <mkthakur2301@gmail.com>
2026-08-16 23:51:30 +0530

    level 2 q227

--- Pattern/level 2/q227.py
+++ Pattern/level 2/q227.py
@@ -1,14 +1,12 @@
 '''
 Enter nth number to print series: 10
-2 6 12 20 30 42 56 72 90 110
+2 5 11 23 47 95 191 383 767 1535 
 '''
 def print_series(n):
-    num=2
-    diff = 4
+    num = 2
     for i in range(1, n+1):
         print(num, end=' ')
-        num +=diff
-        diff +=2
+        num = num * 2+1
 
 n = int(input("Enter nth number to print series: "))
 print_series(n)
