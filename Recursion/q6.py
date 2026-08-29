2398913bf14374bc2055d5af4af7bd1cd8c556be
Manish <mkthakur2301@gmail.com>
2026-08-30 00:59:58 +0530

    Recursion q7

--- Recursion/q6.py
+++ Recursion/q6.py
@@ -2,10 +2,10 @@
 WAJP to print sum of squares of numbers from
 1 to 100 by using recursion.
 '''
-def getSumOfCube(n):
+def getSumOfSquare(n):
     if n==1:
         return 1
 
-    return n**2 + getSumOfCube(n-1)
+    return n**2 + getSumOfSquare(n-1)
 
-print(getSumOfCube(100))
+print(getSumOfSquare(100))
