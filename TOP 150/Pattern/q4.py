eb15d5dedcbd4034f6e4754591851cdf4bb903d1
Manish <mkthakur2301@gmail.com>
2026-09-10 12:10:30 +0530

    Top 150 pattern q4

---
+++ TOP 150/Pattern/q4.py
@@ -0,0 +1,29 @@
+'''
+Enter no. of rows: 9
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+'''
+def  print_pattern(n):
+    mid = n//2+1
+    pattern_size = mid 
+    for i in range(1, n+2):
+        for j in range(1, pattern_size+1):
+            print('* ', end='')
+        print()
+
+        if i<mid:
+            pattern_size -=1
+        else:
+            pattern_size +=1
+
+
+n = int(input("Enter no. of rows: "))
+print_pattern(n)
