3fd4526c247f657107ce69dfabe5feccf520fdbd
Manish <mkthakur2301@gmail.com>
2026-09-10 12:22:51 +0530

    Top 150 pattern q7

---
+++ TOP 150/Pattern/q7.py
@@ -0,0 +1,25 @@
+'''
+Enter no. of rows: 7
+            *   
+          *   *   
+        *   *   *   
+      *   *   *   *   
+    *   *   *   *   *   
+  *   *   *   *   *   *   
+*   *   *   *   *   *   *  
+'''
+def  print_pattern(n):
+    space = n-1
+    pattern_size = 1 
+    for i in range(1, n+1):
+        for j in range(1, space+1):
+            print('  ', end='')
+        for j in range(1, pattern_size+1):
+            print('*   ', end='')
+        print()
+        space -=1
+        pattern_size +=1
+
+
+n = int(input("Enter no. of rows: "))
+print_pattern(n)
