cb2c50e04084d87bdbbf86aa39b0d1c833a0db08
Manish <mkthakur2301@gmail.com>
2026-09-10 12:04:35 +0530

    Top 150 pattern q1

---
+++ TOP 150/Pattern/q1.py
@@ -0,0 +1,24 @@
+'''
+Enter no. of rows: 7
+*
+**
+***
+****
+*****
+******
+*******
+********
+'''
+
+def  print_pattern(n):
+    pattern_size = 1
+
+    for i in range(1, n+2):
+        for j in range(1, pattern_size+1):
+            print('*', end='')
+        pattern_size +=1
+        print()
+
+
+n = int(input("Enter no. of rows: "))
+print_pattern(n)
